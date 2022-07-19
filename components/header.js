1295d68aac6ce6b1e41c3e2f58a6f380a1906a61
hardik <hardik@gmail.com>
2022-07-19 15:34:09 +0530

    header component

---
+++ components/header.js
@@ -0,0 +1,31 @@
+import { ImFacebook, ImYoutube, ImTwitter } from "react-icons/im";
+
+const Header = () => {
+  return (
+    <header className="bg-grey-50">
+      <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
+        <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
+          <input type="text" className="input-text" placeholder="search.." />
+        </div>
+        <div className="shrink w-80 sm:order-2">
+          <a className="font-bold uppercase text3-xl">Design</a>
+        </div>
+        <div className="w-96 order-3 flex justify-center ">
+          <div className="flex gap-6">
+            <a href="">
+              <ImFacebook color="blue" />
+            </a>
+            <a href="">
+              <ImTwitter color="skyblue" />
+            </a>
+            <a href="">
+              <ImYoutube color="red" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
