61a467a19d44bf64f8525e0a016071f6a21dade9
hardik <hardik@gmail.com>
2022-07-20 18:24:24 +0530

    footer UI

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,44 @@
+import { ImFacebook, ImYoutube, ImTwitter } from "react-icons/im";
+import Link from "next/link";
+import NewsLatter from "./_child/newslatter";
 const Footer = () => {
-  return <div className="text-3xl">Footer</div>;
+  const bg = {
+    background: "url('/images/footer.png') no-repeat",
+    backgroundPosition: "bottom left",
+  };
+  return (
+    <footer className="bg-gray-50" style={bg}>
+      <NewsLatter />
+      <div className="container mx-auto flex justify-center py-12">
+        <div className="py-5">
+          <div className="flex gap-6 justify-center">
+            <Link href={"/"}>
+              <a>
+                <ImFacebook color="blue" />
+              </a>
+            </Link>
+
+            <Link href={"/"}>
+              <a>
+                <ImTwitter color="skyblue" />
+              </a>
+            </Link>
+
+            <Link href={"/"}>
+              <a>
+                <ImYoutube color="red" />
+              </a>
+            </Link>
+          </div>
+          <p className="py-5 text-gray-400">
+            Copyright @2022 All rights reserved | This template is made with By
+            ___
+          </p>
+          <p className="text-gray-400 text-center"> Terms & Condition</p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
