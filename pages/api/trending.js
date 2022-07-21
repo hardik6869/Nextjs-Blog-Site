cdb0554469f493c49734180f5b51401d2fbcaf64
hardik <hardik@gmail.com>
2022-07-21 12:41:08 +0530

    APIs

---
+++ pages/api/trending.js
@@ -0,0 +1,8 @@
+import data from "./data";
+
+const handler = (req, res) => {
+  const { Trending } = data;
+  if (Trending) return res.status(200).json(Trending);
+  return res.status(404).json({ error: "Data Not Found" });
+};
+export default handler;
