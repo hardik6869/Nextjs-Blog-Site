8ad60ed498b194b3e6de1dd073baae1f8df6b18c
hardik6869 <sorthiyahardik6869@gmail.com>
2022-08-04 15:34:50 +0530

    Environment Setup

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    BASEURL: "http://localhost:3000/",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
